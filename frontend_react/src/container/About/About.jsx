246badc4e90a91a72b6b4cccdb7d9bd3bf7da77f
floatingnahid22 <nahid.uap.github@gmail.com>
2022-07-08 15:35:22 +0600

    sanity and react pushed

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { client, urlFor } from "../../client";
 import { images } from "../../constants";
 import "./About.scss";
 
@@ -7,11 +9,6 @@ const abouts = [
     title: "Web Development",
     description: "I am a good web developer.",
     imgUrl: images.about01,
-  },
-  {
-    title: "Web Design",
-    description: "I am a good web developer.",
-    imgUrl: images.about02,
   },
   {
     title: "Frontend Development",
@@ -23,12 +20,26 @@ const abouts = [
     description: "I am a good web developer.",
     imgUrl: images.about04,
   },
+  {
+    title: "MERN Stack",
+    description: "I am a good web developer.",
+    imgUrl: images.about02,
+  },
 ];
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
   return (
     <>
       <h2 className="head-text">
-        I Know that <span>Good Work</span> <br />
+        I Know that <span>Good Development</span> <br />
         means <span>Good Business</span>
       </h2>
 
@@ -41,7 +52,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
