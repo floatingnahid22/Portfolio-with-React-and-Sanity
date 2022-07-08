241545bd62eb50034c62cdae11529825d6993874
floatingnahid22 <nahid.uap.github@gmail.com>
2022-07-08 11:05:27 +0600

    file and folder structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,5 +1,17 @@
-const App = () => {
-  return <div>App</div>;
-};
+import "./App.scss";
+import { Navbar } from "./components";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
 
 export default App;
