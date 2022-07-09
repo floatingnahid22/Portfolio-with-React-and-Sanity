2b2a62a240a51ee5732d83fb0af6b2887d734203
floatingnahid22 <nahid.uap.github@gmail.com>
2022-07-09 13:42:03 +0600

    social link updated

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,44 @@
-import { BsInstagram, BsTwitter } from "react-icons/bs";
+import { BsGithub, BsInstagram } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a
+        target="_blank"
+        title="GitHub"
+        rel="noopener noreferrer nofollow"
+        href="https://github.com/floatingnahid22"
+      >
+        {" "}
+        <BsGithub />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a
+        target="_blank"
+        title="Facebook"
+        rel="noopener noreferrer nofollow"
+        href="https://web.facebook.com/nahidhossain1184/"
+      >
+        {" "}
+        <FaFacebookF />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a
+        target="_blank"
+        title="Facebook"
+        rel="noopener noreferrer nofollow"
+        href="https://www.instagram.com/imnahidhossain/"
+      >
+        {" "}
+        <BsInstagram />
+      </a>
     </div>
   </div>
 );
 
 export default SocialMedia;
+
+// <a target="_blank" title="GitHub" rel="noopener noreferrer nofollow" href="https://github.com"></a>
