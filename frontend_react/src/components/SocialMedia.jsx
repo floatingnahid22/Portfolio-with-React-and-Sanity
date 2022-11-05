8bcd8264e08a5df046a6ac5aa44b6591640e763c
floatingnahid22 <nahid.uap.github@gmail.com>
2022-11-05 18:32:44 +0600

    social media section edit

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,8 +1,23 @@
-import { BsGithub, BsInstagram } from "react-icons/bs";
+import {
+  BsFillCloudArrowDownFill,
+  BsGithub,
+  BsInstagram,
+} from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => (
   <div className="app__social">
+    <div>
+      <a
+        target="_blank"
+        title="Resume"
+        rel="noopener noreferrer nofollow"
+        href="https://drive.google.com/file/d/14Kx2K9sz5u8-JAheOFZz0_KiuqjzRSpU/view?usp=sharing"
+      >
+        {" "}
+        <BsFillCloudArrowDownFill />
+      </a>
+    </div>
     <div>
       <a
         target="_blank"
@@ -28,7 +43,7 @@ const SocialMedia = () => (
     <div>
       <a
         target="_blank"
-        title="Facebook"
+        title="Instagram"
         rel="noopener noreferrer nofollow"
         href="https://www.instagram.com/imnahidhossain/"
       >
