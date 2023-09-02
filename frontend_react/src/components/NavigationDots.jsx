f9fddf51e6ee6a710d3d4d22e32e1f4982ed6435
floatingnahid22 <nahid.uap.github@gmail.com>
2023-09-02 13:35:21 +0600

    update node-sass and browser

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -4,6 +4,7 @@ const NavigationDots = ({ active }) => (
   <div className="app__navigation">
     {["home", "about", "work", "skills", "testimonial", "contact"].map(
       (item, index) => (
+        // eslint-disable-next-line jsx-a11y/anchor-has-content
         <a
           href={`#${item}`}
           key={item + index}
